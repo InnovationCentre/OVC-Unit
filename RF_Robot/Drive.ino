c6f63ba4a14dc64adc25d01391d8d06ce1d7285d
Mike <deroodemike@hotmail.com>
2015-08-12 14:37:55 +0200

    Cleaned code

--- RF_Robot/Drive.ino
+++ RF_Robot/Drive.ino
@@ -3,7 +3,7 @@
  * a value of 255 isfull forward and a value of 0 is full reverse. The motors are
  * disables automatically after 2 seconds.
  *
- *  @param address Address of the wheel
+ * @param address Address of the wheel
  * @param driveSpeed Speed of the motor
  */
 void drive(byte address, int driveSpeed)
@@ -32,15 +32,15 @@ void drive(int leftSpeed, int rightSpeed) {
  *
  * @param distance Distance in milimeters
  */
-void DistanceDrive(int distance) //drive a distance straight in mm
+void DistanceDrive(int distance) 
 {
 	int EncCalc = PPT * (distance / (PI * WheelDiameter));
 	encodeReset();
 	Serial.println(EncCalc);
-	do {                                                       // Start loop to drive motors forward
+	do { 
 		drive(DriveSpeed, DriveSpeed);
-		encoder(ENCODERONE);                                             // Calls a function that reads and displays value of encoder 1 to LCD03
-		encoder(ENCODERTWO);                                             // Calls a function that reads and displays value of encoder 2 to LCD03
+		encoder(ENCODERONE);
+		encoder(ENCODERTWO);
 	} while (EncCalc > encoder(ENCODERTWO) || EncCalc > encoder(ENCODERONE));
 	stopMotor();
 }
@@ -48,7 +48,7 @@ void DistanceDrive(int distance) //drive a distance straight in mm
 /**
  * Stops the motors instantly.
  */
-void stopMotor() {                                          // Function to stop motors
+void stopMotor() {
 	drive(StopSpeed, StopSpeed);
 }
 
@@ -67,62 +67,62 @@ void Turn(int degree, int dir)
 	{
 		do {                                                       // Start loop to drive motors forward
 			drive(128 + TurnConst, 128 - TurnConst);
-			encoder(ENCODERONE);                                             // Calls a function that reads and displays value of encoder 1 to LCD03
-			encoder(ENCODERTWO);                                             // Calls a function that reads and displays value of encoder 2 to LCD03
+			encoder(ENCODERONE);    
+			encoder(ENCODERTWO);                  
 		} while (TurnCalc >= encoder(ENCODERONE) || -TurnCalc <= encoder(ENCODERTWO));
 	}
 	else if (dir == 0)
 	{
 		do {                                                       // Start loop to drive motors forward
 			drive(128 - TurnConst, 128 + TurnConst);
-			encoder(ENCODERONE);                                             // Calls a function that reads and displays value of encoder 1 to LCD03
-			encoder(ENCODERTWO);                                             // Calls a function that reads and displays value of encoder 2 to LCD03
+			encoder(ENCODERONE);
+			encoder(ENCODERTWO);   
 		} while (TurnCalc >= encoder(ENCODERTWO) || -TurnCalc <= encoder(ENCODERONE));
 	}
 	stopMotor();
 }
 
-void SpeedControlReset() {                                       // This function disables the speedcontrol
-	writeToAddress(0x30);                                         // Putting the value 0x30 to disable speed control
+void SpeedControlReset() {									// This function disables the speedcontrol
+	writeToAddress(0x30);
 }
 
 void SpeedControlEnable()
 { // This function enables the speedcontrol
-	writeToAddress(0x31);                                         // Putting the value 0x31 to enable speed control
+	writeToAddress(0x31);
 }
 
-void encodeReset() {                                       // This function resets the encoder values to 0
-	writeToAddress(0x20);                                         // Putting the value 0x20 to reset encoders
+void encodeReset() {										// This function resets the encoder values to 0
+	writeToAddress(0x20); 
 }
 
 void writeToAddress(byte message) {
 	Wire.beginTransmission(MD25ADDRESS);
 	Wire.write(CMD);
-	Wire.write(message);                                         // Putting the value 0x20 to reset encoders
+	Wire.write(message); 
 	Wire.endTransmission();
 	delay(100);
 }
 
-long encoder(byte encoderAddress) {                        // Function to read and display value of encoder 1 as a long
-	Wire.beginTransmission(MD25ADDRESS);                      // Send byte to get a reading from encoder 1
+long encoder(byte encoderAddress) {							// Function to read and display value of encoder 1 as a long
+	Wire.beginTransmission(MD25ADDRESS);					// Send byte to get a reading from encoder 1
 	Wire.write(encoderAddress);
 	Wire.endTransmission();
 
-	Wire.requestFrom(MD25ADDRESS, 4);                         // Request 4 bytes from MD25
-	while (Wire.available() < 4);                             // Wait for 4 bytes to arrive
-	long poss1 = Wire.read();                                 // First byte for encoder 1, HH.
+	Wire.requestFrom(MD25ADDRESS, 4);						// Request 4 bytes from MD25
+	while (Wire.available() < 4);							// Wait for 4 bytes to arrive
+	long poss1 = Wire.read();								// First byte for encoder 1, HH.
 	poss1 <<= 8;
-	poss1 += Wire.read();                                     // Second byte for encoder 1, HL
+	poss1 += Wire.read();									// Second byte for encoder 1, HL
 	poss1 <<= 8;
-	poss1 += Wire.read();                                     // Third byte for encoder 1, LH
+	poss1 += Wire.read();									// Third byte for encoder 1, LH
 	poss1 <<= 8;
-	poss1 += Wire.read();                                    // Fourth byte for encoder 1, LL
+	poss1 += Wire.read();									// Fourth byte for encoder 1, LL
 	Serial.println();
 	Serial.print("Encoder ");
 	Serial.print(encoderAddress);
 	Serial.print(" = ");
 	Serial.println(poss1, DEC);
-	delay(50);                                                // Wait for everything to make sure everything is sent
+	delay(50);												// Wait for everything to make sure everything is sent
 
 	return (poss1);
 }
--- RF_Robot/RF_Robot.ino
+++ RF_Robot/RF_Robot.ino
@@ -32,29 +32,29 @@ int OUT_Value3 = 0;
 int OUT_Checksum = 0;
 
 //Motor shield controller
-#define ENL 3     //analog PWM port Left
-#define MotorL4 4 //digital port IN1
-#define MotorL5 5 //digital port IN2
-#define ENR 8     //analog PWM port Right
-#define MotorR6 6 //digital port IN3
-#define MotorR7 7 //digital port IN4
+#define ENL 3     											//analog PWM port Left
+#define MotorL4 4 											//digital port IN1
+#define MotorL5 5 											//digital port IN2
+#define ENR 8     											//analog PWM port Right
+#define MotorR6 6 											//digital port IN3
+#define MotorR7 7 											//digital port IN4
 
-#define CMD                 (byte)0x10                        // Values of 0 eing sent using write have to be cast as a byte to stop them being misinterperted as NULL
-#define MD25ADDRESS         0x58                              // Address of the MD25
-const byte SPEED1 = (byte)0x00;                        // Byte to send speed to first motor
-const byte SPEED2 = 0x01;                              // Byte to send speed to second motor
-#define ENCODERONE          0x02                              // Byte to read motor encoder 1
-#define ENCODERTWO          0x06                              // Byte to read motor encoder 2
-#define VOLTREAD            0x0A                              // Byte to read battery volts
-#define RESETENCODERS       0x20                              // Byte to reset encoders
+#define CMD                 (byte)0x10						// Values of 0 eing sent using write have to be cast as a byte to stop them being misinterperted as NULL
+#define MD25ADDRESS         0x58							// Address of the MD25
+const byte SPEED1 = (byte)0x00;								// Byte to send speed to first motor
+const byte SPEED2 = 0x01;									// Byte to send speed to second motor
+#define ENCODERONE          0x02							// Byte to read motor encoder 1
+#define ENCODERTWO          0x06							// Byte to read motor encoder 2
+#define VOLTREAD            0x0A							// Byte to read battery volts
+#define RESETENCODERS       0x20							// Byte to reset encoders
 
-#define DriveSpeed 250  //standard drive speed
-#define DriveReverseSpeed 4  //standard drive speed
-#define StopSpeed 128   //standard stop speed (255 full forward, 0 full reverse)
+#define DriveSpeed 250										//standard drive speed
+#define DriveReverseSpeed 4									//standard drive speed
+#define StopSpeed 128										//standard stop speed (255 full forward, 0 full reverse)
 #define TurnConst 30
-#define PPT 360 //360 pulses per turn
-#define WheelDiameter 100 //100mm diameter
-#define WheelWidth 470 //distance between middle of wheels
+#define PPT 360												//360 pulses per turn
+#define WheelDiameter 100									//100mm diameter
+#define WheelWidth 470										//distance between middle of wheels
 
 #define powerLed 3
 #define drivingLed 4
